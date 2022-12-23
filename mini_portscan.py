30c334f86837a0adc80c94db93c2926680caa654
Mahmut P <109034196+C0derByM4H6301@users.noreply.github.com>
2022-12-23 19:02:17 +0300

    Update mini_portscan.py

--- mini_portscan.py
+++ mini_portscan.py
@@ -24,9 +24,15 @@ def get_service_name(port):
 ip_address = input("IP adresini girin: ")
 start_port = int(input("Başlangıç portunu girin: "))
 end_port = int(input("Bitiş portunu girin: "))
-open_ports = scan_ports(ip_address, start_port, end_port)
 
-print("Açık portlar:")
-for port in open_ports:
-    service_name = get_service_name(port)
-    print("Port: {}, Service: {}".format(port, service_name))
+for port in range(start_port, end_port + 1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    try:
+        s.connect((ip_address, port))
+        print("Taranan port: {}, Durum: Açık, Servis: {}".format(port, get_service_name(port)))
+        s.shutdown(1)
+        s.close()
+    except Exception:
+        print("Taranan port: {}, Durum: Kapalı".format(port))
+#Teşekkürler.
